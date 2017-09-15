57f978642a276a3e8b8be967a5ebab2ec00bf6d6
Benjamin G <Benjamin G>
2017-09-15 12:38:00 -0400

    hello world test

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
+const app = express()
+
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+    res.send('Hello World!')
 });
 
 module.exports = router;
