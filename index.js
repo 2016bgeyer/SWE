6e687434a03c35aef739c31241239a5c24fec14a
Benjamin G <Benjamin G>
2017-09-15 14:34:15 -0400

    added variables in path

--- index.js
+++ index.js
@@ -11,35 +11,17 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+//leaving this alone so that
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
 app.get('/cool', function(request, response) {
-	response.send(cool());
+    response.send(cool());
 });
 
-app.get('/times', function(request, response) {
-	var result = ''
-	var times = process.env.TIMES || 5
-	for (i=0; i , times; i++)
-		result += i + ' ';
-	response.send(result);
-});
-
-app.get('/db', function (request, response) {
-	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query('SELECT * FROM test_table', function(err, result) {
-			done();
-			if (err) {
-				console.error(err);
-				respnse.send("Error " + err);
-			}
-			else {
-				response.render('pages/db', {results: result.rows} );
-			}
-		});
-	});
+app.get('/:var1/:var2', function(request, response) {
+    response.send(req.params);
 });
 
 app.listen(app.get('port'), function() {
