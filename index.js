451004f1917ecd1aff4f3467ccc817e5bee12791
Benjamin G <Benjamin G>
2017-09-19 04:54:09 -0400

    quite done

--- index.js
+++ index.js
@@ -126,6 +126,7 @@ app.get('*', function(req, res, next) {
 });
 
 
+/*
 //ERROR HANDLING!!
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -143,4 +144,4 @@ app.use(function(err, req, res, next) {
     // render the error page
     res.status(err.status || 500);
     res.render('error');
-});
+});*/
