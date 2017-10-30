37e77cc4a7bbe76e1791794aa30dd17cadba8c49
Ben's Desktop <bengeyer@live.com>
2017-10-29 23:01:41 -0400

    first HW5 test

--- index.js
+++ index.js
@@ -4,13 +4,30 @@ const nock = require('nock');
 const fetch=require('node-fetch');
 const app = express();
 const pg = require('pg');
+const Canvas = require("canvas");
+const cloud = require("d3-cloud-master/");
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
+
+const firebase = require("firebase");
+// Initialize Firebase
+const fireConfig = {
+    apiKey: "AIzaSyCOXM_GoJ2Iod--inOSZ7ADgf6E1ol1Ifg",
+    authDomain: "database-868b8.firebaseapp.com",
+    databaseURL: "https://database-868b8.firebaseio.com",
+    projectId: "database-868b8",
+    storageBucket: "database-868b8.appspot.com",
+    messagingSenderId: "115155038268"
+};0
+firebase.initializeApp(fireConfig);
+
+let database = firebase.database();
+database.ref('users/' + 'Josh').set(
+    {username: 'Ben', email: 'b@gmail.com'
+    });
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -28,48 +45,51 @@ app.set('view engine', 'ejs');
 //#1     http://localhost:5000/count/patient.reaction.reactionmeddrapt.exact
 app.get('/count/:info', function(req, res) {
     fetch(`https://api.fda.gov/drug/event.json?count=${req.params.info}`)
-    .then(function(dataRes){
-        return dataRes.json();
-    }).then(function(json){
-        res.send(json.results);
-    }).catch(function(err) {
-        fetch(`https://api.fda.gov/drug/event.json?count=${req.params.info}`)
-    .then(function(dataRes){
+        .then(function(dataRes){
             return dataRes.json();
         }).then(function(json){
+            res.send(json.results);
+    }).catch(function(err) {
+        fetch(`https://api.fda.gov/drug/event.json?count=${req.params.info}`)
+            .then(function(dataRes){
+                return dataRes.json();
+            }).then(function(json){
             res.send(json.results);
         });
         console.log("err", err);
         throw new Error("An error occurred");
     });
 });
+
 //#2    http://localhost:5000/stats/patient.reaction.reactionmeddrapt.exact
 app.get('/stats/:info', function(req, res){
     fetch(`https://api.fda.gov/drug/event.json?count=${req.params.info}`)
-    .then(function(dataRes){
-        return dataRes.json();
-    }).then(function(json){
-        return json.results;
-    }).then(function(data){
-        res.send(`${data[0].term} occurs the most at ${data[0].count} times and ${data[data.length-1].term} occurs the least at ${data[data.length-1].count} times.`);
-    }).catch(function(err) {
+        .then(function(dataRes){
+            return dataRes.json();
+        }).then(function(json){
+            return json.results;
+        }).then(function(data){
+            res.send(`${data[0].term} occurs the most at ${data[0].count} times and ${data[data.length-1].term} occurs the least at ${data[data.length-1].count} times.`);
+        }).catch(function(err) {
         console.log("err", err);
         throw new Error("An error occurred");
     });
 });
+
 class streak{
     constructor(month, size){
         this.month = month;
         this.size = size;
     }
 }
+
 //#3    finds the month with the most adverse drug events: http://localhost:5000/commonmonth
 app.get('/commonmonth', function(req, res){
-    let current = new streak(0, 0);
-    let longest = new streak(0, 0);
-    fetch('https://api.fda.gov/drug/event.json?count=receiptdate')
+    let current;
+    let longest;
+    Promise.all([fetch('https://api.fda.gov/drug/event.json?count=receiptdate'), current=new streak(0,0), longest=new streak(0,0)])
         .then(function(dataRes) {
-            return dataRes.json();
+            return dataRes[0].json();
         }).then(function(json) {
             for(let date of json.results){
                 let time = parseInt(date.time.substring(4,6));
@@ -91,87 +111,69 @@ app.get('/commonmonth', function(req, res){
     });
 });
 
-/*// var search;
-let search = fetch('https://api.fda.gov/drug/event.json?count=patient.patientsex').then(function(dataRes) {
-    return dataRes.json();
-});*/
-
-class Reaction{
+class Symptoms{
     constructor(json){
         this.json = json;
     }
-    showReactions(){
+    showSymptoms(){
         let text = [];
-        for(let reaction of search[0].patient.reaction)
-            text.push(reaction.reactionmeddrapt);
+        for(let symptom of search[0].patient.reaction)
+            text.push(symptom.reactionmeddrapt);
         return text;
     }
 }
 
 let search;
-//#4       SEARCH FUNCTION : http://localhost:5000/2/search/reactionmeddrapt/headache
+//#4       SEARCH FUNCTION: http://localhost:5000/2/search/reactionmeddrapt/headache
 app.get('/:count/:query/:field/:name', function(req, res){
     fetch(`https://api.fda.gov/drug/event.json?${req.params.query}=patient.reaction.${req.params.field}:"${req.params.name}"&limit=${req.params.count}`)
-        .then(function(dataRes){
-            return dataRes.json();
-        }).then(function(json){
+.then(function(dataRes){
+        return dataRes.json();
+    }).then(function(json){
         search = json.results;
-        let reactions=new Reaction(search);
-        return reactions;
-        }).then(function(reactions){
-            res.send(reactions.showReactions());
+        let symptoms=new Symptoms(search);
+        return symptoms;
+    }).then(function(symptoms){
+        res.send(symptoms.showSymptoms());
     });
 });
 
-
-//#5    Stores last Search's reactions: http://localhost:5000/Search_reactions
-app.get('/Search_reactions',function(req,res){
+//#5    Stores last Search's symptoms: http://localhost:5000/Search_symptoms
+app.get('/Search_symptoms',function(req,res){
     if(search) {
-        let reactions = new Reaction(search);
-        res.send("First Reactions: " +reactions.showReactions());
+        let symptoms = new Symptoms(search);
+        res.send("First Symptoms: " +symptoms.showSymptoms());
     }
     else
         res.send("A search has not been performed yet.");
 });
 
-/*let reactions = {};
-fetch('https://api.fda.gov/drug/event.json?search=_exists_:patient.reaction')
-    .then(function(dataRes) {
-        return dataRes.json();
-    }).then(function(json) {
-    reactions = json.results[0].patient.reaction;
-}).catch(function(err) {
-    console.log("err", err);
-    throw new Error("An error occurred");
-});*/
-
-
-
-//#6    Adds reactions to latest search. http://localhost:5000/addReaction/coughing/
-app.get('/addReaction/:reaction', function(req, res){
+//#6    Adds symptoms to latest search. http://localhost:5000/addSymptom/coughing/
+app.get('/addSymptom/:symptom', function(req, res){
     if(search){
-        search[0].patient.reaction.push({"reactionmeddrapt":req.params.reaction});
-        let reaction = new Reaction(search);
-        res.send(reaction.showReactions());
+        search[0].patient.reaction.push({"reactionmeddrapt":req.params.symptom});
+        let symptom = new Symptoms(search);
+        res.send(symptom.showSymptoms());
     }
     else
         res.send("A search has not been performed yet.");
 });
 
 let tweets = {};
-
 setInterval(() => {
     if (search) {
         fetch(`http://api.flutrack.org/?s=${search[1].patient.reaction[0].reactionmeddrapt}OR${search[1].patient.reaction[1].reactionmeddrapt}`)
-            .then(function (dataRes) {
-                return dataRes.json();
-
-            }).then(function (json) {
-                console.log("interval");
+        .then(function (dataRes) {
+            return dataRes.json();
+        }).then(function (json) {
             tweets = json;
-        })
+        }).catch(function(err) {
+            console.log("err", err);
+            throw new Error("An error occurred");
+        });
     }
-    }, 1000);
+}, 1000);
+
 class twitter{
     constructor(json){
         this.json = json;
@@ -183,6 +185,7 @@ class twitter{
         return text;
     }
 }
+
 //#7       Searches flutrack for symptoms specified: http://localhost:5000/tweets/
 app.get('/tweets', function(req, res) {
     if (tweets&&search) {
@@ -193,6 +196,14 @@ app.get('/tweets', function(req, res) {
         res.send("A search has not been performed yet.");
 });
 
+
+app.get('/wordcloud/', function(req, res) {
+    // if (tweets&&search) {
+        res.render('pages/wordcloud');
+    // }
+    // else
+    //     res.send("A search has not been performed yet.");
+});
 
 //leaving this alone
 app.get('/', function(request, response) {
